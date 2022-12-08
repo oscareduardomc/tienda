ae32d9e6a5e96d2ac18079dd98f7175081fec0a2
Julio Caballero <jucecal24@hotmail.com>
2022-12-07 20:14:44 -0600

    vista login

---
+++ src/views/login.php
@@ -0,0 +1,7 @@
+<form action="" method="post">
+    <h3>login now</h3>
+    <input type="email" name="email" required placeholder="enter email" class="box">
+    <input type="password" name="password" required placeholder="enter password" class="box">
+    <input type="submit" name="submit" class="btn" value="login now">
+    <p>don't have an account? <a href="register.php">register now</a></p>
+</form>
