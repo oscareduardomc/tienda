d47bdfd3404625ddd13e1ab8c335f098e044ca45
Julio Caballero <jucecal24@hotmail.com>
2022-12-07 21:31:01 -0600

    Cambios

---
+++ src/controller/controladorProducto.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_GET['logout'])) {
+    unset($user_id);
+    session_destroy();
+    header('location:index.php');
+};
+
+if (isset($_POST['add_to_cart'])) {
+
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+    $product_stock = $_POST['product_stock'];
+    $product_id = $_POST['product_id'];
