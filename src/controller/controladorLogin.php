2e4b5ba161500bcc71127fee91fc54d6f3e66086
Antoni <84816441+Antoni2223@users.noreply.github.com>
2022-12-07 21:00:20 -0600

    controladorLogin finish
    finishe controlador

---
+++ src/controller/controladorLogin.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['submit'])) {
+
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+
+    $select = mysqli_query($conn, "SELECT * FROM `user_info` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+    if (mysqli_num_rows($select) > 0) {
+        $row = mysqli_fetch_assoc($select);
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['user_rol'] = $row['rol'];
+        header('location:index.php');
+    } else {
+        $message[] = 'incorrect password or email!';
+    }
+}
