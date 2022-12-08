1e1bcc23974cb6a5c4b42dd8fbbb77b0dfd93090
Julio Caballero <jucecal24@hotmail.com>
2022-12-07 20:18:29 -0600

    vista registro

---
+++ src/views/registro.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+    <h3>register now</h3>
+    <input type="text" name="name" required placeholder="enter username" class="box">
+    <input type="email" name="email" required placeholder="enter email" class="box">
+    <input type="password" name="password" required placeholder="enter password" class="box">
+    <input type="password" name="cpassword" required placeholder="confirm password" class="box">
+    <input type="submit" name="submit" class="btn" value="register now">
+    <p>already have an account? <a href="login.php">login now</a></p>
+</form>
